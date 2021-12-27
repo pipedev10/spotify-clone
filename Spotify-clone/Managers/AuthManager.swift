0f4ed3b5ffb73745f20df6cb6ee55678c68e468d
Felipe Carrasco <felipe.carrasco@codelegends.cl>
2021-12-26 23:44:56 -0300

    02 feature: auth set up

--- Spotify-clone/Managers/AuthManager.swift
+++ Spotify-clone/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = ""
+        static let clientSecret = ""
+    }
+    
     private init(){}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://github.com/pipedev10"
+        let base = "https://accounts.spotify.com/authorize"
+        let urlComplete = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: urlComplete)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +43,20 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get Token
+    }
+    
+    public func refreshAccessToken(){
+        
+    }
+    
+    private func cacheToken(){
+        
     }
 }
